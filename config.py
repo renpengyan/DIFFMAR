86050c2f33c27b6f205b7a81db05c80c6989049b
wwww <3255954757@qq.com>
2025-06-25 10:44:47 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+from types import SimpleNamespace
+
+def get_config():
+    config = SimpleNamespace()
+
+    # 数据路径
+    config.train_data = 'data/train'
+
+    # 超参数
+    config.batch_size = 1
+    config.lr = 2e-5
+    config.epochs = 300
+    config.T = 10  # diffusion steps
+    config.latent_dim = 128
+
+    return config
