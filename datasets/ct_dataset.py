2932770cd9677bf1702108b820395e1ca423aea4
wwww <3255954757@qq.com>
2025-06-26 13:11:17 +0800

    Update ct_dataset.py

--- datasets/ct_dataset.py
+++ datasets/ct_dataset.py
@@ -1,44 +1,35 @@
+import random
 import os
 import cv2
 import torch
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
-import random
 
 def load_gray(path):
     img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    if img is None:
-        raise FileNotFoundError(f"Cannot load image: {path}")
     return img.astype('float32') / 255.0
 
 class CTMetalDataset(Dataset):
     def __init__(self, root_dir):
         self.root_dir = root_dir
+        self.x0_files = sorted(os.listdir(os.path.join(root_dir, 'x0')))
+        self.mask_files = sorted(os.listdir(os.path.join(root_dir, 'mask')))
+        self.xT_files = sorted(os.listdir(os.path.join(root_dir, 'xT')))
+        self.xLI_files = sorted(os.listdir(os.path.join(root_dir, 'xLI')))
+
         self.to_tensor = transforms.ToTensor()
 
-        x0_dir = os.path.join(root_dir, 'x0')
-        xT_dir = os.path.join(root_dir, 'xT')
-        xLI_dir = os.path.join(root_dir, 'xLI')
-        mask_dir = os.path.join(root_dir, 'mask')
-
-        # 只保留同时在 x0/xT/xLI 中存在的文件
-        all_x0 = sorted([f for f in os.listdir(x0_dir) if f.endswith('.png')])
-        xT_set = set(os.listdir(xT_dir)) if os.path.exists(xT_dir) else set()
-        xLI_set = set(os.listdir(xLI_dir)) if os.path.exists(xLI_dir) else set()
-
-        self.x0_files = [f for f in all_x0 if f in xT_set and f in xLI_set]
-        self.mask_files = sorted([f for f in os.listdir(mask_dir) if f.endswith('.png')])
-
     def __len__(self):
-        return len(self.x0_files)
+        return len(self.x0_files) 
 
     def __getitem__(self, idx):
-        fname = self.x0_files[idx]
+        x0_path = os.path.join(self.root_dir, 'x0', self.x0_files[idx])
+        xT_path = os.path.join(self.root_dir, 'xT', self.xT_files[idx])
+        xLI_path = os.path.join(self.root_dir, 'xLI', self.xLI_files[idx])
 
-        x0_path = os.path.join(self.root_dir, 'x0', fname)
-        xT_path = os.path.join(self.root_dir, 'xT', fname)
-        xLI_path = os.path.join(self.root_dir, 'xLI', fname)
-        mask_path = os.path.join(self.root_dir, 'mask', random.choice(self.mask_files))
+  
+        mask_file = random.choice(self.mask_files)
+        mask_path = os.path.join(self.root_dir, 'mask', mask_file)
 
         x0 = load_gray(x0_path)
         xT = load_gray(xT_path)
@@ -51,5 +42,4 @@ class CTMetalDataset(Dataset):
             'xLI': self.to_tensor(xLI),
             'mask': self.to_tensor(mask)
         }
-
 
